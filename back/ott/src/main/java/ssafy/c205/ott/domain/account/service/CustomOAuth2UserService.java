a81c309b96a106c7311ac248ff210d3c9bdeaa13
JiEung2 <wldmd5007@naver.com>
2024-07-22 17:58:14 +0900

    feat: "CustomOAuth2UserService 추가"

---
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/service/CustomOAuth2UserService.java
@@ -0,0 +1,87 @@
+package ssafy.c205.ott.domain.account.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import ssafy.c205.ott.common.oauth.dto.*;
+import ssafy.c205.ott.domain.account.entity.Member;
+import ssafy.c205.ott.domain.account.entity.MemberRole;
+import ssafy.c205.ott.domain.account.repository.MemberRepository;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+
+    public CustomOAuth2UserService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println("oAuth2User = " + oAuth2User);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+
+        if (registrationId.equals("naver")) {
+
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+
+        } else if (registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+
+        } else if (registrationId.equals("kakao")) {
+
+            oAuth2Response = new KakaoResponse(oAuth2User.getAttributes());
+
+        } else {
+
+            return null;
+        }
+
+        //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듦
+        String sso = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        Member existData = memberRepository.findByName(sso);
+
+        if (existData == null) {
+
+            Member member = Member.builder()
+                    .name(oAuth2Response.getName())
+                    .sso(sso)
+                    .email(oAuth2Response.getEmail())
+                    .role(MemberRole.USER)
+                    .build();
+
+            memberRepository.save(member);
+
+            MemberDTO memberDTO = new MemberDTO();
+            memberDTO.setName(member.getName());
+            memberDTO.setSso(member.getSso());
+            memberDTO.setEmail(member.getEmail());
+            memberDTO.setRole(member.getRole());
+
+            return new CustomOAuth2User(memberDTO);
+        } else {
+
+            existData.updateEmailAndName(oAuth2Response.getEmail(), oAuth2Response.getName());
+
+            memberRepository.save(existData);
+
+            MemberDTO memberDTO = new MemberDTO();
+            memberDTO.setName(existData.getName());
+            memberDTO.setSso(existData.getSso());
+            memberDTO.setEmail(existData.getEmail());
+            memberDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(memberDTO);
+        }
+
+    }
+
+}
