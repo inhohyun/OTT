4a8ce79ffa77e31e1bdf503e93c64070f566c4a8
root <root@DESKTOP-ITGGK6I>
2024-07-24 01:46:10 +0900

    fix: eslint and prettier 삭제

--- front/ott/src/App.jsx
+++ front/ott/src/App.jsx
@@ -1,29 +1,28 @@
-import GlobalStyle from './styles/GlobalStyles';
-import Layout from './styles/Layout';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './styles/tailwind.css'
-import Login from './pages/Login/Login'
-import UserPage from './pages/UserPage/Userpage'
-import SurveyStart from './pages/Survey/SurveyStart';
-import SurveyIng from './pages/Survey/SurveyIng';
+import GlobalStyle from "./styles/GlobalStyles";
+import Layout from "./styles/Layout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/tailwind.css";
+import Login from "./pages/Login/Login";
+import UserPage from "./pages/UserPage/Userpage";
+import SurveyStart from "./pages/Survey/SurveyStart";
+import SurveyIng from "./pages/Survey/SurveyIng";
 
-import MainPage from './pages/MainPage/MainPage';
-
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Layout>
-        <Router> 
+        <Router>
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/userPage" element={<UserPage />} />
             <Route path="/survey_start" element={<SurveyStart />} />
             <Route path="/survey_ing" element={<SurveyIng />} />
-            <Route path='/mainpage' element={<MainPage />} />
+            <Route path="/mainpage" element={<MainPage />} />
           </Routes>
-        </Router>     
+        </Router>
       </Layout>
     </>
   );
