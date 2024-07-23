b724ddc96aec7eab0c4284299b5dcb5d69aee17f
inhohyun <inhohyun36@gmail.com>
2024-07-23 13:38:02 +0900

    update: main_logo 추가

--- front/ott/src/pages/Login/Login.jsx
+++ front/ott/src/pages/Login/Login.jsx
@@ -1,13 +1,20 @@
+import './Login.css';
+import kakaoLoginImg from '../../assets/images/login/kakao_login.png'; // 이미지 경로 수정 필요
 
 const Login = () => {
-
-    return (
-     
-        <div>
-            <h1>test</h1>
-        </div>
-      );
-    
-}
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <div className="logo">ㅇㅅㅅ</div>
+        <h1>당신만의 온라인 옷 관리 서비스</h1>
+        <p>가지고 있는 옷 관리부터 스타일 조합, 친구들의 추천까지 전부 받아보세요</p>
+        <button className="kakao-login-button">
+          <img src={kakaoLoginImg} alt="Kakao Login" />
+          카카오 로그인
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
