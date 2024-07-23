0fc739e669750e4e5c597989b19e0229b609561e
inhohyun <inhohyun36@gmail.com>
2024-07-23 15:29:30 +0900

    update: tailwind 세팅

---
+++ front/ott/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html","./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#fee500",
+        black: "#000",
+        gray: "#242424",
+      },
+      spacing: {},
+      fontFamily: {
+        "reggae-one": "'Reggae One'",
+        "zen-kaku-gothic-antique": "'Zen Kaku Gothic Antique'",
+      },
+    },
+    fontSize: {
+      inherit: "inherit",
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
