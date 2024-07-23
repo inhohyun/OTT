0b74aa81fb4bfe39577ebda95dd81b9f96356885
gahyoenj <rkgus678@gmail.com>
2024-07-23 21:55:28 +0900

    feat: 팔로우 없을 때 화면 디자인 구현
    S11P12C205-250

---
+++ front/ott/src/components/feed/FeedNoFollow.jsx
@@ -0,0 +1,27 @@
+import logo from '../../assets/icons/main.logo.png';
+
+const FeedNoFollow = () => {
+    return (
+        <div className="flex flex-col items-center justify-center rounded-2xl shadow-lg max-w-md mx-auto mt-40" style={{ width: '45%' }}>
+            <div className="w-full bg-violet-200 rounded-t-2xl p-6">
+                <div className="flex items-center justify-center w-20 h-20 bg-white rounded-full mx-auto">
+                    <img src={logo} alt="No Follow" className="w-10 h-10"/>
+                </div>
+            </div>
+            <div className="flex flex-col items-center w-full bg-white rounded-b-2xl p-6">
+                <div className="text-center">
+                    <p className="text-lg font-bold text-slate-500 mb-4">현재 팔로우하는 <br></br>사람이 없습니다!</p>
+                    <p className="text-sm text-slate-500 mb-6">사람들을 팔로우해 <br></br>스타일 추천을 받아보세요!</p>
+                </div>
+                <div className="flex justify-around w-full">
+                    <button className="bg-violet-200 text-violet-700 font-semibold py-2 px-4 rounded-full shadow-md font-dohyeon"
+                    style={{ fontFamily: 'dohyeon' }}>추천받기</button>
+                    <button className="bg-violet-200 text-violet-700 font-semibold py-2 px-4 rounded-full shadow-md font-dohyeon"
+                    style={{ fontFamily: 'dohyeon' }}>검색하기</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FeedNoFollow;
