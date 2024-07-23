770a75819440f40cd19b3842a60d860b4a73baa8
JiEung2 <wldmd5007@naver.com>
2024-07-23 17:54:20 +0900

    feat: "member id 조회, 닉네임 존재 확인 메서드 추가"
    S11P12C205-11

--- back/ott/src/main/java/ssafy/c205/ott/domain/account/repository/MemberRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package ssafy.c205.ott.domain.account.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ssafy.c205.ott.domain.account.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findBySso(String Sso);
+    Optional<Member> findById(Long id);
+    Boolean existsByNickname(String nickname);
 }
