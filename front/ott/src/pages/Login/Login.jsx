f1821a16609b25ffcc6edd9cc403216b3be668cd
inhohyun <inhohyun36@gmail.com>
2024-07-24 10:34:29 +0900

    feat: 네이버 구글 로그인 버튼 구현

--- front/ott/src/pages/Login/Login.jsx
+++ front/ott/src/pages/Login/Login.jsx
@@ -1,11 +1,21 @@
 import { useCallback } from "react";
 import mainIcon from '../../assets/icons/main.logo.png';
 import kakao from '../../assets/images/login/kakao_login.png';
+import naver from '../../assets/images/login/naver_login.png';
+import google from '../../assets/images/login/google_login.png';
 import backgroundImage from '../../assets/images/background_image_survey.png';
 
 const Login = () => {
   const onKakaoLoginClick = useCallback(() => {
     // Please sync "설문 실시 안내" to the project
+  }, []);
+
+  const onNaverLoginClick = useCallback(() => {
+    // 네이버 로그인 클릭 시 동작
+  }, []);
+
+  const onGoogleLoginClick = useCallback(() => {
+    // 구글 로그인 클릭 시 동작
   }, []);
 
   return (
@@ -30,12 +40,29 @@ const Login = () => {
             <p className="m-0">친구들의 추천까지 전부 받아보세요</p>
           </div>
         </section>
+        <div className="absolute top-[603px] left-[125px] flex flex-col items-center gap-[16px] z-[2]">
         <img
-          className="absolute top-[603px] left-[125px] rounded-md w-[350px] cursor-pointer z-[2]"
-          alt="카카오 로그인"
-          src={kakao}
-          onClick={onKakaoLoginClick}
+            className="rounded-md w-[350px] h-[45px] cursor-pointer"
+            alt="카카오 로그인"
+            src={kakao}
+            onClick={onKakaoLoginClick}
+          /> 
+        <img
+            className="rounded-md w-[350px] h-[45px] cursor-pointer"
+            alt="네이버 로그인"
+            src={naver}
+            onClick={onNaverLoginClick}
+          />
+         
+          <img
+          className="rounded-md w-[350px] h-[45px] cursor-pointer"
+          alt="구글 로그인"
+          src={google}
+          onClick={onGoogleLoginClick}
         />
+          
+         
+        </div>
       </div>
     </div>
   );
