9da769c993da8a849fc096d060e5b3c5882f65a4
JiEung2 <wldmd5007@naver.com>
2024-07-24 17:54:09 +0900

    feat: "닉네임 제약 추가"
    S11P12C205-7

--- back/ott/src/main/java/ssafy/c205/ott/domain/account/dto/request/MemberUpdateRequestDto.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/dto/request/MemberUpdateRequestDto.java
@@ -1,8 +1,9 @@
 package ssafy.c205.ott.domain.account.dto.request;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.annotations.processing.Pattern;
 import ssafy.c205.ott.common.entity.MemberTag;
 import ssafy.c205.ott.common.entity.PublicStatus;
 import ssafy.c205.ott.domain.account.entity.BodyType;
@@ -16,8 +17,8 @@ public class MemberUpdateRequestDto {
     //Todo: 예외처리 구현
     private Long memberId;
 
-//    @Pattern(regexp = "^[a-zA-Z_]+$", message = "닉네임은 영어와 밑줄(_)만 사용할 수 있습니다.")
-//    @Size(max = 25, message = "닉네임은 25자 이하로 작성해야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z_]+$", message = "닉네임은 영어와 밑줄(_)만 사용할 수 있습니다.")
+    @Size(max = 25, message = "닉네임은 25자 이하로 작성해야 합니다.")
     private String nickname;
     private String phoneNumber;
     private String introduction;
