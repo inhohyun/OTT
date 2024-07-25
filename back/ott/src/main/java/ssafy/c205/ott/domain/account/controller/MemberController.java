24595d7bb00269e1e381fdb594ada8945b47f10a
JiEung2 <wldmd5007@naver.com>
2024-07-25 14:10:39 +0900

    feat: "member 조회 컨트롤러 구현"
    S11P12C205-11

---
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/controller/MemberController.java
@@ -0,0 +1,31 @@
+package ssafy.c205.ott.domain.account.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ssafy.c205.ott.domain.account.dto.request.MemberRequestDto;
+import ssafy.c205.ott.domain.account.dto.request.MemberUpdateRequestDto;
+import ssafy.c205.ott.domain.account.dto.response.DeleteMemberSuccessDto;
+import ssafy.c205.ott.domain.account.dto.response.MemberInfoDto;
+import ssafy.c205.ott.domain.account.dto.response.UpdateMemberSuccessDto;
+import ssafy.c205.ott.domain.account.dto.response.ValidateNicknameSuccessDto;
+import ssafy.c205.ott.domain.account.service.MemberReadService;
+import ssafy.c205.ott.domain.account.service.MemberValidator;
+import ssafy.c205.ott.domain.account.service.MemberWriteService;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberReadService memberReadService;
+    private final MemberWriteService memberWriteService;
+    private final MemberValidator memberValidator;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberInfoDto> getMember(@PathVariable Long id) {
+        MemberInfoDto memberInfoDto = memberReadService.memberSearch(MemberRequestDto.builder().id(id).build());
+        return ResponseEntity.ok(memberInfoDto);
+    }
+
+}
