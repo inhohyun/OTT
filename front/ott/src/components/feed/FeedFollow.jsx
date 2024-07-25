6c33c2df6264de4230d9bea6a4623172095915f6
전가현 <rkgus678@gmail.com>
2024-07-25 11:27:51 +0900

    design: 모달 크기 조정, 컴포넌트 간격 조정

--- front/ott/src/components/feed/FeedFollow.jsx
+++ front/ott/src/components/feed/FeedFollow.jsx
@@ -48,7 +48,7 @@ const FeedFollow = () => {
 
     return (
         <div className="relative flex flex-col items-start w-full p-5 space-y-5">
-            <style>{`
+<style>{`
                 .scrollbar-hide::-webkit-scrollbar {
                     display: none;
                 }
@@ -56,17 +56,17 @@ const FeedFollow = () => {
                     -ms-overflow-style: none;
                     scrollbar-width: none;
                 }
+                .lookbook-container, .show-more-button {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    margin: 0 2px; /* Further reduced margin */
+                }
                 .button-no-style {
                     background: none;
                     border: none;
                     padding: 0;
-                    margin: 0;
                     cursor: pointer;
-                    position: relative;
-                    top: -70px;
-                }
-                .button-no-style img {
-                    display: block;
                 }
             `}</style>
             {followers.map((follower, index) => (
@@ -78,15 +78,15 @@ const FeedFollow = () => {
                             className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white shadow-lg p-1 rounded-full z-10 w-8 h-8"
                             style={{ backgroundImage: `url(${leftArrow})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}
                         ></button>
-                        <div ref={scrollRefs.current[index]} className="flex space-x-4 overflow-x-auto py-4 scrollbar-hide">
+                        <div ref={scrollRefs.current[index]} className="flex overflow-x-auto py-4 scrollbar-hide">
                             {allLookbooks[follower].slice(0, visibleLookbooks[follower]).map((_, lookbookIndex) => (
-                                <div key={lookbookIndex} className="flex items-center justify-center">
+                                <div key={lookbookIndex} className="lookbook-container">
                                     <Lookbook />
                                 </div>
                             ))}
                             {visibleLookbooks[follower] < allLookbooks[follower].length && (
-                                <div className="flex items-center justify-center">
-                                    <button onClick={() => showMore(follower)} className="button-no-style">
+                                <div className="show-more-button">
+                                    <button onClick={() => showMore(follower)} className="relative top-[-70px] bg-transparent border-none p-0 m-0 cursor-pointer">
                                         <img src={plus} alt="Show more" className="w-6 h-6" />
                                     </button>
                                 </div>
