95c5ecfcb7a6b4dc3cf0c74f8f8e0a1afe4b4a88
JiEung2 <wldmd5007@naver.com>
2024-07-25 14:05:16 +0900

    fix: "Member 조회 기능 변경(active 계정만)"
    S11P12C205-11

--- back/ott/src/main/java/ssafy/c205/ott/domain/account/repository/MemberRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/repository/MemberRepository.java
@@ -1,12 +1,13 @@
 package ssafy.c205.ott.domain.account.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ssafy.c205.ott.domain.account.entity.ActiveStatus;
 import ssafy.c205.ott.domain.account.entity.Member;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findBySso(String Sso);
-    Optional<Member> findById(Long id);
+    Optional<Member> findByIdAndActiveStatus(Long id, ActiveStatus activeStatus);
     Boolean existsByNickname(String nickname);
 }
