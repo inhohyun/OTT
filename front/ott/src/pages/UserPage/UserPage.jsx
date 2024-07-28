6c91791fc95b715d5c7e853901381a6f685df601
inhohyun <inhohyun36@gmail.com>
2024-07-28 16:37:33 +0900

    refator: 유저 페이지 Navbar 코드 분리

--- front/ott/src/pages/UserPage/UserPage.jsx
+++ front/ott/src/pages/UserPage/UserPage.jsx
@@ -7,47 +7,7 @@ import Following from "@/components/userPage/Following";
 import backgroundImage from "../../assets/images/background_image_main.png";
 import lockIcon from "../../assets/icons/lockicon.png";
 import settingIcon from "../../assets/icons/Setting_icon.png";
-
-const NavBar = ({ activeComponent, setActiveComponent }) => {
-  const postNumber = 100;
-  const follwerNumber = 100;
-  const followingNumber = 100;
-
-  return (
-    <nav className="w-full text-center flex justify-around text-lg mt- font-dohyeon">
-      <button
-        className={`bg-transparent border-none text-[rgba(0,0,0,0.5)] py-2 px-5 cursor-pointer rounded-full ${
-          activeComponent === "posts" ? "bg-violet-200" : ""
-        }`}
-        onClick={() => setActiveComponent("posts")}
-        aria-label="게시글"
-        style={{ fontFamily: "dohyeon" }}
-      >
-        {postNumber} 게시글
-      </button>
-      <button
-        className={`bg-transparent border-none text-[rgba(0,0,0,0.5)] py-2 px-5 cursor-pointer rounded-full ${
-          activeComponent === "followers" ? "bg-violet-200" : ""
-        }`}
-        onClick={() => setActiveComponent("followers")}
-        aria-label="팔로워"
-        style={{ fontFamily: "dohyeon" }}
-      >
-        {follwerNumber} 팔로워
-      </button>
-      <button
-        className={`bg-transparent border-none text-[rgba(0,0,0,0.5)] py-2 px-5 cursor-pointer rounded-full ${
-          activeComponent === "following" ? "bg-violet-200" : ""
-        }`}
-        onClick={() => setActiveComponent("following")}
-        aria-label="팔로잉"
-        style={{ fontFamily: "dohyeon" }}
-      >
-        {followingNumber} 팔로잉
-      </button>
-    </nav>
-  );
-};
+import NavBar from "@/components/userPage/NavBar";
 
 const UserPage = () => {
   const [activeComponent, setActiveComponent] = useState("posts");
