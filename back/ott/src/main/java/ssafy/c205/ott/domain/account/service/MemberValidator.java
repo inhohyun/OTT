1ac4327048bcbfbfcfb001767f71b5e20786c14d
JiEung2 <wldmd5007@naver.com>
2024-07-29 17:55:59 +0900

    feat: follow 검증 기능 구현

--- back/ott/src/main/java/ssafy/c205/ott/domain/account/service/MemberValidator.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/service/MemberValidator.java
@@ -2,8 +2,12 @@ package ssafy.c205.ott.domain.account.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ssafy.c205.ott.domain.account.dto.request.FollowRequestDto;
 import ssafy.c205.ott.domain.account.dto.response.ValidateNicknameSuccessDto;
+import ssafy.c205.ott.domain.account.entity.Member;
 import ssafy.c205.ott.domain.account.exception.MemberNicknameDuplicateException;
+import ssafy.c205.ott.domain.account.exception.MemberNotFoundException;
+import ssafy.c205.ott.domain.account.exception.SelfFollowException;
 import ssafy.c205.ott.domain.account.repository.MemberRepository;
 
 @Service
@@ -18,4 +22,10 @@ public class MemberValidator {
         }
         return new ValidateNicknameSuccessDto();
     }
+
+    public void validateFollow(FollowRequestDto followRequestDto) {
+        if (followRequestDto.getRequestMemberId().equals(followRequestDto.getTargetMemberId()))
+            throw new SelfFollowException();
+    }
+
 }
