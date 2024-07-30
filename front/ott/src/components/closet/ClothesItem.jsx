631bf2475e7dcaec91fb78926046b151890bb065
piilll <spchoi1998@naver.com>
2024-07-30 09:20:08 +0900

    design: 옷장 페이지 터치 스크롤 구현 및 하단 스크롤바 제거

--- front/ott/src/components/closet/ClothesItem.jsx
+++ front/ott/src/components/closet/ClothesItem.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
 const ClothesItem = ({ item }) => {
   return (
     <div className="bg-white p-4 rounded-lg shadow-md relative">
-      <img src={item.frontImage} alt={item.category} className="w-full h-32 object-cover rounded-t-lg" />
+      <img
+        src={item.frontImage}
+        alt={item.category}
+        className="w-full h-32 object-cover rounded-t-lg"
+      />
       {item.backImage && (
-        <img src={item.backImage} alt={item.category} className="w-full h-32 object-cover rounded-t-lg mt-2" />
+        <img
+          src={item.backImage}
+          alt={item.category}
+          className="w-full h-32 object-cover rounded-t-lg mt-2"
+        />
       )}
     </div>
   );
