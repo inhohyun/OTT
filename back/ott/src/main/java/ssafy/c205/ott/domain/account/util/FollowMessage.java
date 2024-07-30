eafd769ab968f6355acb460e646fccd4d7731df1
JiEung2 <wldmd5007@naver.com>
2024-07-30 16:27:04 +0900

    feat: follow 요청 수락 거절을 위한 메시지 추가

--- back/ott/src/main/java/ssafy/c205/ott/domain/account/util/FollowMessage.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/util/FollowMessage.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum FollowMessage {
     FOLLOW_REQUEST_MESSAGE("팔로우 요청을 완료하였습니다."),
     FOLLOW_SUCCESS_MESSAGE("팔로우를 성공했습니다."),
-    UNFOLLOW_SUCCESS_MESSAGE("팔로우를 취소했습니다."),;
+    UNFOLLOW_SUCCESS_MESSAGE("팔로우를 취소했습니다."),
+    FOLLOW_ACCEPT_MESSAGE("팔로우 요청을 수락했습니다."),
+    FOLLOW_REJECT_MESSAGE("팔로우 요청을 거절했습니다.");
 
     private final String message;
 
