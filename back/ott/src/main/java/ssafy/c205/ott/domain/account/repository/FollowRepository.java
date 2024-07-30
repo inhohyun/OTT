18f497ca993f8c962cf38efc877e27d264706880
JiEung2 <wldmd5007@naver.com>
2024-07-30 11:29:55 +0900

    feat: follow 삭제 메서드 추가
    S11P12C205-27

--- back/ott/src/main/java/ssafy/c205/ott/domain/account/repository/FollowRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/repository/FollowRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Optional<Follow> findByToMemberIdAndFromMemberId(Long toMemberId, Long fromMemberId);
+    void deleteByToMemberAndFromMember(Member toMember, Member fromMember);
 }
