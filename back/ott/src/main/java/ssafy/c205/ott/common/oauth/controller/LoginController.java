00eafda72df834379bf51b77c67893d3332924d8
JiEung2 <wldmd5007@naver.com>
2024-08-01 14:37:07 +0900

    fix: 기본 api 설정

--- back/ott/src/main/java/ssafy/c205/ott/common/oauth/controller/LoginController.java
+++ back/ott/src/main/java/ssafy/c205/ott/common/oauth/controller/LoginController.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 @Slf4j
 public class LoginController {
 
