28eec7806222d9ebff5fa66c7125f436d91d1cb2
JiEung2 <wldmd5007@naver.com>
2024-08-01 11:53:53 +0900

    fix: 쿠키 생성 ResponseCookie를 통한 생성으로 변경

--- back/ott/src/main/java/ssafy/c205/ott/common/oauth/service/CookieService.java
+++ back/ott/src/main/java/ssafy/c205/ott/common/oauth/service/CookieService.java
@@ -1,19 +1,17 @@
 package ssafy.c205.ott.common.oauth.service;
 
-import jakarta.servlet.http.Cookie;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CookieService {
-    public Cookie createCookie(String key, String value) {
-
-        Cookie cookie = new Cookie(key, value); //쿠키 생성
-        cookie.setMaxAge(60 * 60 * 60); //쿠키가 살아있을 시간 설정
-        cookie.setSecure(true); //https 환경에서만 쿠키가 사용될 수 있게 하는 설정
-        cookie.setPath("/"); //쿠키가 보일 위치는 모든 전역 위치
-        cookie.setHttpOnly(true); //js가 쿠키를 가져가지 못하게 설정
-        cookie.setComment("SameSite=None; Secure");
-
-        return cookie;
+    public ResponseCookie createResponseCookie(String key, String value) {
+        return ResponseCookie.from(key, value)
+                .maxAge(60 * 60 * 60)
+                .secure(true)
+                .path("/")
+                .httpOnly(true)
+                .sameSite("None")  // SameSite=None 설정
+                .build();
     }
 }
