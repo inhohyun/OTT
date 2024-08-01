983816cef917f20765a704f6b50185e8489fb676
JiEung2 <wldmd5007@naver.com>
2024-08-01 12:13:32 +0900

    feat: 로그 추가

--- back/ott/src/main/java/ssafy/c205/ott/common/oauth/controller/LoginController.java
+++ back/ott/src/main/java/ssafy/c205/ott/common/oauth/controller/LoginController.java
@@ -1,23 +1,28 @@
 package ssafy.c205.ott.common.oauth.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class LoginController {
 
     @GetMapping("/login/naver")
     public String naverLogin() {
+        log.info("naverLogin");
         return "redirect:/oauth2/authorization/naver";
     }
 
     @GetMapping("/login/kakao")
     public String kakaoLogin() {
+        log.info("kakaoLogin");
         return "redirect:/oauth2/authorization/kakao";
     }
 
     @GetMapping("/login/google")
     public String googleLogin() {
+        log.info("googleLogin");
         return "redirect:/oauth2/authorization/google";
     }
 }
