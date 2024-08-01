95cb42908ec61588062309ebb528f0c7d2db0d41
JiEung2 <wldmd5007@naver.com>
2024-08-01 14:59:54 +0900

    fix: 기본 requestmapping 수정

--- back/ott/src/main/java/ssafy/c205/ott/common/oauth/controller/LoginController.java
+++ back/ott/src/main/java/ssafy/c205/ott/common/oauth/controller/LoginController.java
@@ -2,9 +2,11 @@ package ssafy.c205.ott.common.oauth.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 @Slf4j
 public class LoginController {
 
