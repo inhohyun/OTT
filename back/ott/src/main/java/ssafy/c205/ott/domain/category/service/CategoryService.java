e6a716dc5149c78d12c22fd252597a6ff1548870
JiEung2 <wldmd5007@naver.com>
2024-08-02 17:57:04 +0900

    feat: category register 리팩토링

--- back/ott/src/main/java/ssafy/c205/ott/domain/category/service/CategoryService.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/category/service/CategoryService.java
@@ -4,37 +4,59 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ssafy.c205.ott.common.entity.ClosetCategory;
-import ssafy.c205.ott.domain.category.dto.CategoryDto;
+import ssafy.c205.ott.domain.category.dto.CategoryRequestDto;
+import ssafy.c205.ott.domain.category.dto.DeleteCategorySuccessDto;
 import ssafy.c205.ott.domain.category.dto.RegisterCategorySuccessDto;
 import ssafy.c205.ott.domain.category.entity.Category;
+import ssafy.c205.ott.domain.category.exception.CategoryNotFoundException;
 import ssafy.c205.ott.domain.category.repository.CategoryRepository;
 import ssafy.c205.ott.domain.category.repository.ClosetCategoryRepository;
 import ssafy.c205.ott.domain.closet.entity.Closet;
 import ssafy.c205.ott.domain.closet.repository.ClosetRepository;
-import ssafy.c205.ott.domain.closet.service.ClosetService;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class CategoryService {
+    // Todo: Closet Not Fount 예외처리
     private final CategoryRepository categoryRepository;
     private final ClosetCategoryRepository closetCategoryRepository;
     private final ClosetRepository closetRepository;
 
-    public RegisterCategorySuccessDto registerCategory(long closetId, String name) {
-        Category category = categoryRepository.findByName(name);
-        if (category == null) {
-            log.error("Category with name {} not found", name);
-            category = new Category(name);
-            categoryRepository.save(category);
-        }
-        Closet closet = closetRepository.findById(closetId).orElseThrow();
+    public RegisterCategorySuccessDto registerCategory(CategoryRequestDto categoryRequestDto) {
+        Category category = findOrCreateCategoryByName(categoryRequestDto.getName());
+        Closet closet = findClosetById(categoryRequestDto.getId());
+        saveClosetCategory(closet, category);
+
+        return buildRegisterCategorySuccessDto(category);
+    }
+
+    private Category findOrCreateCategoryByName(String name) {
+        return categoryRepository.findByName(name)
+                .orElseGet(() -> createAndSaveCategory(name));
+    }
+
+    private Category createAndSaveCategory(String name) {
+        log.error("Category with name {} not found, creating a new one.", name);
+        Category category = new Category(name);
+        categoryRepository.save(category);
+        return category;
+    }
+
+    private Closet findClosetById(long closetId) {
+        return closetRepository.findById(closetId)
+                .orElseThrow();
+    }
+
+    private void saveClosetCategory(Closet closet, Category category) {
         ClosetCategory closetCategory = ClosetCategory.builder()
                 .closet(closet)
                 .category(category)
                 .build();
         closetCategoryRepository.save(closetCategory);
+    }
 
+    private RegisterCategorySuccessDto buildRegisterCategorySuccessDto(Category category) {
         return RegisterCategorySuccessDto.builder()
                 .categoryId(category.getId())
                 .build();
