a708b2662e32b8cd80a02f8490c6b00a8befe9e3
inhohyun <inhohyun36@gmail.com>
2024-08-02 15:46:58 +0900

    update: OAuth 설정 수정

--- front/ott/public/service-worker.js
+++ front/ott/public/service-worker.js
@@ -1,15 +1,4 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating.');
-});
-
+// TODO : PWA 공부하고 수정하기
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
