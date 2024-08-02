b0d1d356650eb7828e7b80b3d41a2aa8cdbd01dd
JiEung2 <wldmd5007@naver.com>
2024-08-02 17:57:31 +0900

    feat: category validator 생성

---
+++ back/ott/src/main/java/ssafy/c205/ott/domain/category/service/CategoryValidator.java
@@ -0,0 +1,10 @@
+package ssafy.c205.ott.domain.category.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryValidator {
+
+}
