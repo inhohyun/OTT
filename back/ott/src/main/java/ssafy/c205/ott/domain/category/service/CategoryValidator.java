31793c652d14ad1b8e6a2c2537b839c77283e37b
JiEung2 <wldmd5007@naver.com>
2024-08-05 14:37:14 +0900

    feat: 카테고리 이름이 중복되었는지 확인하는 validator 추가

--- back/ott/src/main/java/ssafy/c205/ott/domain/category/service/CategoryValidator.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/category/service/CategoryValidator.java
@@ -2,9 +2,19 @@ package ssafy.c205.ott.domain.category.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ssafy.c205.ott.domain.category.dto.CategoryRequestDto;
+import ssafy.c205.ott.domain.category.exception.CategoryNameDuplicationException;
+import ssafy.c205.ott.domain.category.repository.CategoryRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryValidator {
 
+    private final CategoryRepository categoryRepository;
+
+    public void validateCategoryName(CategoryRequestDto categoryRequestDto) {
+        if (categoryRepository.existsByName(categoryRequestDto.getName())) {
+            throw new CategoryNameDuplicationException();
+        }
+    }
 }
