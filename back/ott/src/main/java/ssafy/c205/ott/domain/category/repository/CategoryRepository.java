6b0eb12c188c5881d549e2ee6b332598ff8519dc
JiEung2 <wldmd5007@naver.com>
2024-08-05 09:33:19 +0900

    fix: category 조회 타입 Optional로 변경
    조회했을 때 없을 수도 있기 때문에

--- back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/CategoryRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/CategoryRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ssafy.c205.ott.domain.category.entity.Category;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    public Category findByName(String name);
+    Optional<Category> findByName(String name);
 }
