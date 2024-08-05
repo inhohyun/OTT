ae088047ceb89257317ac6fede022b5e50c6f319
JiEung2 <wldmd5007@naver.com>
2024-08-05 14:36:25 +0900

    feat: 카테고리 이름으로 존재하는지 확인하는 메서드 추가

--- back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/CategoryRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/CategoryRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
+    boolean existsByName(String name);
+
 }
