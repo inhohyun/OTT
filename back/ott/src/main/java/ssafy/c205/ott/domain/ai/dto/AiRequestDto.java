200ee0118ae3706014da3845a7594ecf41a7dd9c
JaeHunJang <wuddlaa@gmail.com>
2024-08-06 01:28:57 +0900

    feat: add AiRequestDto
    S11P12C205-304

---
+++ back/ott/src/main/java/ssafy/c205/ott/domain/ai/dto/AiRequestDto.java
@@ -0,0 +1,20 @@
+package ssafy.c205.ott.domain.ai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+@Builder
+@AllArgsConstructor
+public class AiRequestDto {
+    private Long memberId;
+    private String modelImagePath;
+    private String clothImagePath;
+    private String modelType;
+    private Integer category;
+    private Float scale;
+    private Integer sample;
+}
