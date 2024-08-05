11c7ad2b47bd6fdc629ceb7dd403267014771f89
JiEung2 <wldmd5007@naver.com>
2024-08-05 15:07:21 +0900

    fix: existsByName에서 existsByNameAndClosetId로 변경
    옷장의 카테고리에 대해 존재하는지 파악해야하므로 옷장 id를 포함해서 검색

--- back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/CategoryRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/CategoryRepository.java
@@ -10,6 +10,6 @@ import java.util.Optional;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
-    boolean existsByName(String name);
+    boolean existsByNameAndClosetId(String name, Long closetId);
     List<Category> findByClosetId(Long id);
 }
