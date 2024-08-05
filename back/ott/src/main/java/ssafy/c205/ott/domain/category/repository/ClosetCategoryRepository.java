8cb4900e51c916453e4395507f5dd8c777b1af4f
JiEung2 <wldmd5007@naver.com>
2024-08-05 13:20:19 +0900

    feat: ClosetCategory에 findByClosetId 생성

--- back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/ClosetCategoryRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/ClosetCategoryRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ssafy.c205.ott.common.entity.ClosetCategory;
 
+import java.util.List;
+
 @Repository
 public interface ClosetCategoryRepository extends JpaRepository<ClosetCategory, Long> {
-
+    List<ClosetCategory> findByClosetId(Long closetId);
 }
