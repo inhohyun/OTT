a2ec517390589ef2d1a59be5d5870014a3cde9c4
inhohyun <inhohyun36@gmail.com>
2024-08-05 14:10:53 +0900

    fix: OAuth useEffect 수정

--- front/ott/src/pages/OAuth/OAuthCallback.jsx
+++ front/ott/src/pages/OAuth/OAuthCallback.jsx
@@ -18,11 +18,15 @@ import { useCookies } from 'react-cookie';
 // OAuthCallback 컴포넌트 정의
 const OAuthCallback = () => {
   const navigate = useNavigate(); // useNavigate 훅을 사용하여 페이지를 이동합니다.
-  const [cookies, setCookie, removeCookie] = useCookies();
+  const [cookies, setCookie, removeCookie] = useCookies(['refresh']);
+
   useEffect(() => {
-    const refreshToken = cookies;
+    const refreshToken = cookies['refresh'];
+    setLocalRefreshToken(refreshToken);
+  }, [cookies]);
+  useEffect(() => {
     // 헤더에서 액세스 토큰 추출
-    console.log('쿠키에 들어있는 값 출력:', cookies);
+    console.log('쿠키에 들어있는 값 출력:', refreshToken);
 
     if (refreshToken) {
       // 각각의 토큰을 로컬 스토리지에 저장합니다.
