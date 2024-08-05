e0306a8ff6798632cfdda0ecaeddc46edca3294d
JiEung2 <wldmd5007@naver.com>
2024-08-05 14:36:48 +0900

    feat: 카테고리를 closetId로 검색하는 메서드 추가

--- back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/CategoryRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/category/repository/CategoryRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ssafy.c205.ott.domain.category.entity.Category;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
     boolean existsByName(String name);
-
+    List<Category> findByClosetId(Long id);
 }
