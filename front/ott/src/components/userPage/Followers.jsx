6a9cb2f48f3df5bdbef6630999df880c98724aeb
inhohyun <inhohyun36@gmail.com>
2024-08-06 16:50:05 +0900

    fix: Follower.jsx useEffect 없는 에러 수정

--- front/ott/src/components/userPage/Followers.jsx
+++ front/ott/src/components/userPage/Followers.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import defaultImage from '../../assets/icons/main.logo.png';
 import { getFollowerList } from '../../api/follower/follower';
 const Followers = (uid) => {
