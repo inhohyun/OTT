46f1ad7b2d7a1c0f73f015954c82ab377e616e1b
piilll <spchoi1998@naver.com>
2024-08-06 11:19:48 +0900

    feat: 옷 상세 정보 조회 axios 연동 완료

--- front/ott/src/components/closet/ClothesDetailModal.jsx
+++ front/ott/src/components/closet/ClothesDetailModal.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Select from 'react-select'; // Import react-select
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faTimes,
-  faEdit,
   faChevronLeft,
   faChevronRight,
 } from '@fortawesome/free-solid-svg-icons';
@@ -14,361 +13,144 @@ const ClothesDetailModal = ({
   clothingItem,
   onEdit,
   onDelete,
-  categories = [],
+  categories,
 }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [editableItem, setEditableItem] = useState(clothingItem || {});
-  const [frontImage, setFrontImage] = useState(clothingItem?.img?.[0] || '');
-  const [backImage, setBackImage] = useState(clothingItem?.img?.[1] || '');
+  const [itemDetails, setItemDetails] = useState(clothingItem);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [category, setCategory] = useState(clothingItem?.category || '');
-  const [publicStatus, setPublicStatus] = useState(
-    clothingItem?.publicStatus || 'PRIVATE'
-  );
-  const [salesStatus, setSalesStatus] = useState(
-    clothingItem?.salesStatus || 'NOT_SALE'
-  );
-  const [gender, setGender] = useState(clothingItem?.gender || 'COMMON');
 
-  useEffect(() => {
-    if (clothingItem) {
-      console.log('Editing clothing item:', clothingItem);
-      setEditableItem(clothingItem);
-      setFrontImage(clothingItem.img?.[0] || '');
-      setBackImage(clothingItem.img?.[1] || '');
-      setCategory(clothingItem.category);
-      setPublicStatus(clothingItem.publicStatus || 'PRIVATE');
-      setSalesStatus(clothingItem.salesStatus || 'NOT_SALE');
-      setGender(clothingItem.gender || 'COMMON');
+  const getGenderText = (gender) => {
+    switch (gender) {
+      case 'MAN':
+        return '남성';
+      case 'WOMAN':
+        return '여성';
+      case 'COMMON':
+        return '남녀공용';
+      default:
+        return '지정되지 않음';
     }
-  }, [clothingItem]);
-
-  useEffect(() => {
-    if (!isOpen) {
-      setIsEditing(false);
-      setCurrentImageIndex(0);
-    }
-  }, [isOpen]);
-
-  if (!isOpen || !clothingItem) return null;
-
-  const customStyles = {
-    control: (provided, state) => ({
-      ...provided,
-      'borderColor': state.isFocused ? 'black' : provided.borderColor,
-      '&:hover': {
-        borderColor: 'black',
-      },
-      'boxShadow': state.isFocused ? '0 0 0 1px black' : provided.boxShadow,
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      backgroundColor: state.isSelected ? '#a78bfa' : 'white',
-      color: state.isSelected ? 'white' : 'black',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    }),
   };
 
-  const handleEditClick = () => {
-    setIsEditing(true);
+  const getPublicStatusText = (publicStatus) => {
+    switch (publicStatus) {
+      case 'PUBLIC':
+        return '공개';
+      case 'PRIVATE':
+        return '비공개';
+      default:
+        return '지정되지 않음';
+    }
   };
 
-  const handleSaveClick = () => {
-    const updatedData = {
-      ...editableItem,
-      category,
-      publicStatus,
-      salesStatus,
-      gender,
-      img: [frontImage, backImage],
+  const getSalesStatusText = (salesStatus) => {
+    switch (salesStatus) {
+      case 'ON_SALE':
+        return 'O';
+      case 'NOT_SALE':
+        return 'X';
+      default:
+        return '지정되지 않음';
+    }
+  };
+
+  useEffect(() => {
+    const fetchItemDetails = async () => {
+      if (clothingItem) {
+        try {
+          const response = await axios.get(
+            `http://192.168.100.89:8080/api/clothes/${clothingItem.clothesId}`
+          );
+          setItemDetails(response.data);
+          console.log(response.data);
+        } catch (error) {
+          console.error('Failed to fetch item details:', error);
+        }
+      }
     };
 
-    console.log('Saving edited data to backend:', updatedData);
+    fetchItemDetails();
+  }, [clothingItem]);
 
-    onEdit(updatedData);
-    setIsEditing(false);
-    onClose();
-  };
-
-  const handleDeleteClick = () => {
-    onDelete(editableItem.id);
-    onClose();
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setEditableItem({ ...editableItem, [name]: value });
-  };
-
-  const handleImageChange = (e, setImage) => {
-    if (e.target.files && e.target.files[0]) {
-      const file = e.target.files[0];
-      const imageUrl = URL.createObjectURL(file);
-      setImage(imageUrl);
-    }
-  };
-
-  const handleNextImage = (e) => {
-    e.stopPropagation();
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 2);
-  };
-
-  const handlePreviousImage = (e) => {
-    e.stopPropagation();
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + 2) % 2);
-  };
-
-  const getCurrentImage = () => {
-    return currentImageIndex === 0 ? frontImage : backImage;
-  };
-
-  const triggerFileInput = () => {
-    const input = document.getElementById(`file-input-${currentImageIndex}`);
-    if (input) {
-      input.click();
-    }
-  };
-
-  const categoryOptions = categories.map((cat) => ({ value: cat, label: cat }));
-  const publicOptions = [
-    { value: 'PUBLIC', label: '공개' },
-    { value: 'PRIVATE', label: '비공개' },
-  ];
-  const salesOptions = [
-    { value: 'ON_SALE', label: '판매 중' },
-    { value: 'NOT_SALE', label: '판매 안 함' },
-  ];
-  const genderOptions = [
-    { value: 'MAN', label: '남성' },
-    { value: 'WOMAN', label: '여성' },
-    { value: 'COMMON', label: '남녀공용' },
-  ];
+  if (!isOpen || !itemDetails) return null;
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full relative">
         <div className="absolute top-3 right-3 flex space-x-2">
-          {!isEditing && (
-            <div
-              onClick={handleEditClick}
-              className="text-gray-600 hover:text-gray-800 cursor-pointer"
-            >
-              <FontAwesomeIcon icon={faEdit} size="lg" />
-            </div>
-          )}
           <div
-            onClick={() => {
-              setIsEditing(false);
-              onClose();
-            }}
+            onClick={onClose}
             className="text-gray-600 hover:text-gray-800 cursor-pointer"
           >
             <FontAwesomeIcon icon={faTimes} size="lg" />
           </div>
         </div>
         <div className="overflow-y-auto max-h-[75vh] p-4">
-          <div
-            className="relative flex justify-center mb-4"
-            onClick={isEditing ? triggerFileInput : undefined}
-          >
-            <div className="w-40 h-60 rounded-lg overflow-hidden relative cursor-pointer">
-              {getCurrentImage() ? (
+          <h2 className="text-xl font-bold">
+            {itemDetails.name || '상세 정보'}
+          </h2>
+          <div className="relative mb-4">
+            {itemDetails.imageUrls && itemDetails.imageUrls.length > 0 ? (
+              <div>
                 <img
-                  src={getCurrentImage()}
-                  alt="Clothing"
-                  className="object-cover w-full h-full"
+                  src={itemDetails.imageUrls[currentImageIndex]}
+                  alt={`옷 이미지 ${currentImageIndex + 1}`}
+                  className="w-full my-2"
                 />
-              ) : (
-                <div className="w-full h-full flex items-center justify-center border-2 border-dashed border-gray-400 rounded-lg">
-                  <label className="text-gray-700">
-                    {currentImageIndex === 0
-                      ? 'Add Front Image'
-                      : 'Add Back Image'}
-                  </label>
-                </div>
-              )}
-            </div>
-            {(isEditing || (frontImage && backImage)) && (
-              <>
-                <button
-                  className="absolute left-0 top-1/2 transform -translate-y-1/2 p-2 bg-gray-200 rounded-full"
-                  onClick={handlePreviousImage}
-                >
-                  <FontAwesomeIcon icon={faChevronLeft} />
-                </button>
-                <button
-                  className="absolute right-0 top-1/2 transform -translate-y-1/2 p-2 bg-gray-200 rounded-full"
-                  onClick={handleNextImage}
-                >
-                  <FontAwesomeIcon icon={faChevronRight} />
-                </button>
-              </>
-            )}
-          </div>
-
-          {/* Hidden file inputs for image upload */}
-          <input
-            type="file"
-            accept="image/*"
-            id="file-input-0"
-            className="hidden"
-            onChange={(e) => handleImageChange(e, setFrontImage)}
-          />
-          <input
-            type="file"
-            accept="image/*"
-            id="file-input-1"
-            className="hidden"
-            onChange={(e) => handleImageChange(e, setBackImage)}
-          />
-
-          <div className="mb-4">
-            {isEditing ? (
-              <>
-                <div className="flex items-center mb-2">
-                  <label className="text-gray-700 mr-2 w-24">카테고리</label>
-                  <Select
-                    value={categoryOptions.find(
-                      (opt) => opt.value === category
-                    )}
-                    onChange={(opt) => setCategory(opt.value)}
-                    options={categoryOptions}
-                    styles={customStyles}
-                  />
-                </div>
-                <div className="flex items-center mb-2">
-                  <label className="text-gray-700 mr-2 w-24">브랜드</label>
-                  <input
-                    type="text"
-                    name="brand"
-                    value={editableItem.brand}
-                    onChange={handleChange}
-                    className="flex-grow p-2 border rounded-lg"
-                  />
-                </div>
-                <div className="flex items-center mb-2">
-                  <label className="text-gray-700 mr-2 w-24">사이즈</label>
-                  <input
-                    type="text"
-                    name="size"
-                    value={editableItem.size}
-                    onChange={handleChange}
-                    className="flex-grow p-2 border rounded-lg"
-                  />
-                </div>
-                <div className="flex items-center mb-2">
-                  <label className="text-gray-700 mr-2 w-24">색상</label>
-                  <input
-                    type="text"
-                    name="color"
-                    value={editableItem.color}
-                    onChange={handleChange}
-                    className="flex-grow p-2 border rounded-lg"
-                  />
-                </div>
-                <div className="flex items-center mb-2">
-                  <label className="text-gray-700 mr-2 w-24">구매처</label>
-                  <input
-                    type="text"
-                    name="purchase"
-                    value={editableItem.purchase}
-                    onChange={handleChange}
-                    className="flex-grow p-2 border rounded-lg"
-                  />
-                </div>
-                <div className="flex items-center mb-2">
-                  <label className="text-gray-700 mr-2 w-24">공개 여부</label>
-                  <Select
-                    value={publicOptions.find(
-                      (opt) => opt.value === publicStatus
-                    )}
-                    onChange={(opt) => setPublicStatus(opt.value)}
-                    options={publicOptions}
-                    styles={customStyles}
-                  />
-                </div>
-                <div className="flex items-center mb-2">
-                  <label className="text-gray-700 mr-2 w-24">판매 여부</label>
-                  <Select
-                    value={salesOptions.find(
-                      (opt) => opt.value === salesStatus
-                    )}
-                    onChange={(opt) => setSalesStatus(opt.value)}
-                    options={salesOptions}
-                    styles={customStyles}
-                  />
-                </div>
-                <div className="flex items-center mb-2">
-                  <label className="text-gray-700 mr-2 w-24">성별</label>
-                  <Select
-                    value={genderOptions.find((opt) => opt.value === gender)}
-                    onChange={(opt) => setGender(opt.value)}
-                    options={genderOptions}
-                    styles={customStyles}
-                  />
-                </div>
-              </>
+                {itemDetails.imageUrls.length > 1 && (
+                  <>
+                    <div
+                      className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-200 p-2 rounded-full"
+                      onClick={() =>
+                        setCurrentImageIndex(
+                          (currentImageIndex -
+                            1 +
+                            itemDetails.imageUrls.length) %
+                            itemDetails.imageUrls.length
+                        )
+                      }
+                    >
+                      <FontAwesomeIcon icon={faChevronLeft} />
+                    </div>
+                    <div
+                      className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-200 p-2 rounded-full"
+                      onClick={() =>
+                        setCurrentImageIndex(
+                          (currentImageIndex + 1) % itemDetails.imageUrls.length
+                        )
+                      }
+                    >
+                      <FontAwesomeIcon icon={faChevronRight} />
+                    </div>
+                  </>
+                )}
+              </div>
             ) : (
-              <>
-                <p>
-                  <strong>카테고리:</strong> {editableItem.category}
-                </p>
-                <p>
-                  <strong>브랜드:</strong> {editableItem.brand}
-                </p>
-                <p>
-                  <strong>사이즈:</strong> {editableItem.size}
-                </p>
-                <p>
-                  <strong>색상:</strong> {editableItem.color}
-                </p>
-                <p>
-                  <strong>구매처:</strong>{' '}
-                  <a
-                    href={editableItem.purchase}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {editableItem.purchase}
-                  </a>
-                </p>
-                <p>
-                  <strong>공개 여부:</strong>{' '}
-                  {publicStatus === 'PUBLIC' ? '공개' : '비공개'}
-                </p>
-                <p>
-                  <strong>판매 여부:</strong>{' '}
-                  {salesStatus === 'ON_SALE' ? '판매 중' : '판매 안 함'}
-                </p>
-                <p>
-                  <strong>성별:</strong>{' '}
-                  {gender === 'MAN'
-                    ? '남성'
-                    : gender === 'WOMAN'
-                      ? '여성'
-                      : '남녀공용'}
-                </p>
-              </>
+              <p>이미지가 없습니다.</p>
             )}
           </div>
-          {isEditing && (
-            <div className="flex justify-center mt-4 space-x-4">
-              <button
-                onClick={handleSaveClick}
-                className="bg-violet-300 text-white px-4 py-2 rounded-lg"
-              >
-                저장
-              </button>
-              <button
-                onClick={handleDeleteClick}
-                className="bg-stone-400 text-white px-4 py-2 rounded-lg"
-              >
-                삭제
-              </button>
-            </div>
-          )}
+          <p>카테고리: {itemDetails.categories || '지정되지 않음'}</p>
+          <p>브랜드: {itemDetails.brand || '지정되지 않음'}</p>
+          <p>사이즈: {itemDetails.size || '지정되지 않음'}</p>
+          <p>색상: {itemDetails.color || '지정되지 않음'}</p>
+          <p>구매처: {itemDetails.purchase || '지정되지 않음'}</p>
+          <p>성별: {getGenderText(itemDetails.gender)}</p>
+          <p>판매 여부: {getSalesStatusText(itemDetails.salesStatus)}</p>
+          <p>공개 여부: {getPublicStatusText(itemDetails.publicStatus)}</p>
+          <p>즐겨찾기: {itemDetails.isLiked ? '예' : '아니오'}</p>
+          <div className="flex justify-center mt-4 space-x-4">
+            <button
+              onClick={() => onEdit(itemDetails)}
+              className="bg-violet-300 text-white px-4 py-2 rounded-lg"
+            >
+              수정
+            </button>
+            <button
+              onClick={() => onDelete(itemDetails.id)}
+              className="bg-red-500 text-white px-4 py-2 rounded-lg"
+            >
+              삭제
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- front/ott/src/pages/Closet/ClosetPage.jsx
+++ front/ott/src/pages/Closet/ClosetPage.jsx
@@ -7,11 +7,8 @@ import ClothesDetailModal from '../../components/closet/ClothesDetailModal';
 import clothesData from './clothesData.js';
 
 const ClosetPage = () => {
-  // 카테고리 기본 설정값 === 전체
   const [selectedCategory, setSelectedCategory] = useState('전체');
-  // 더미데이터 가져오기
   const [clothes, setClothes] = useState(clothesData);
-  // 기본으로 정의되는 카테고리들 6개
   const [categories, setCategories] = useState([
     '전체',
     '상의',
@@ -20,33 +17,26 @@ const ClosetPage = () => {
     '한벌옷',
     '즐겨찾기',
   ]);
-  // 옷 추가 모달 상태
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-  // 옷 상세정보 모달 상태
   const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
-  // 수정하고자 하는 옷 선택
   const [selectedClothing, setSelectedClothing] = useState(null);
 
-  // 옷의 카테고리 변경
   const handleCategoryChange = (newCategory) => {
     setSelectedCategory(newCategory);
   };
 
-  // 카테고리 추가
   const handleAddCategory = (newCategory) => {
     if (!categories.includes(newCategory)) {
       setCategories([...categories, newCategory]);
     }
   };
 
-  // 카테고리 이름 수정
   const handleEditCategory = (oldCategory, newCategoryName) => {
     setCategories((prevCategories) =>
       prevCategories.map((category) =>
         category === oldCategory ? newCategoryName : category
       )
     );
-    // 카테고리 이름 바꿨을 때 새로운 이름으로 옷의 카테고리 변경
     setClothes((prevClothes) =>
       prevClothes.map((item) =>
         item.category === oldCategory
@@ -56,12 +46,10 @@ const ClosetPage = () => {
     );
   };
 
-  // 카테고리 삭제
   const handleDeleteCategory = (categoryToDelete) => {
     setCategories((prevCategories) =>
       prevCategories.filter((category) => category !== categoryToDelete)
     );
-    // 삭제된 카테고리의 옷들 -> 카테고리 전체로 변경
     setClothes((prevClothes) =>
       prevClothes.map((item) =>
         item.category === categoryToDelete
@@ -71,7 +59,6 @@ const ClosetPage = () => {
     );
   };
 
-  // 옷 추가
   const handleAddClothes = (newClothes) => {
     setClothes([...clothes, newClothes]);
     if (!categories.includes(newClothes.category)) {
@@ -79,7 +66,6 @@ const ClosetPage = () => {
     }
   };
 
-  // 옷의 즐겨찾기 토글
   const handleToggleLike = (id) => {
     setClothes(
       clothes.map((item) =>
@@ -88,13 +74,11 @@ const ClosetPage = () => {
     );
   };
 
-  // 옷 사진 눌렀을 때 옷 상세정보 모달 띄우기
   const handleClothesClick = (clothingItem) => {
     setSelectedClothing(clothingItem);
     setIsDetailModalOpen(true);
   };
 
-  // 옷 상세정보 수정
   const handleEditClothes = (updatedClothing) => {
     setClothes(
       clothes.map((item) =>
@@ -104,13 +88,11 @@ const ClosetPage = () => {
     setIsDetailModalOpen(false);
   };
 
-  // 옷 삭제
   const handleDeleteClothes = (id) => {
     setClothes(clothes.filter((item) => item.id !== id));
     setIsDetailModalOpen(false);
   };
 
-  // 카테고리별로 옷 분류
   const filteredClothes =
     selectedCategory === '전체'
       ? clothes
@@ -118,7 +100,6 @@ const ClosetPage = () => {
         ? clothes.filter((item) => item.isLiked)
         : clothes.filter((item) => item.category === selectedCategory);
 
-  // '전체', '즐겨찾기' 제외한 나머지 카테고리들
   const filteredCategories = categories.filter(
     (category) => category !== '전체' && category !== '즐겨찾기'
   );
