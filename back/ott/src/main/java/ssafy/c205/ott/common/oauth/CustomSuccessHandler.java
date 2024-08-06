2df9a59bd7dd6abae31a2a193440a7ae0bdab620
JiEung2 <wldmd5007@naver.com>
2024-08-06 11:40:20 +0900

    fix: currentId가 null 값으로 전달되는 오류 수정

--- back/ott/src/main/java/ssafy/c205/ott/common/oauth/CustomSuccessHandler.java
+++ back/ott/src/main/java/ssafy/c205/ott/common/oauth/CustomSuccessHandler.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
