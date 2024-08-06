df55c0b7e97bae013459f6478ac6f2555834e1c4
JiEung2 <wldmd5007@naver.com>
2024-08-06 11:58:05 +0900

    refactor: myId를 찾는 로직 분리로 인한 코드 변경

--- back/ott/src/main/java/ssafy/c205/ott/domain/account/controller/MemberController.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/controller/MemberController.java
@@ -30,12 +30,15 @@ public class MemberController {
 
     @GetMapping("/my")
     public ApiResponse<MemberIdDto> getMember(@AuthenticationPrincipal CustomOAuth2User currentMember) {
-        return ApiResponse.success(MemberIdDto.builder().id(memberRepository.findBySso(currentMember.getUsername()).getId()).build());
+        return ApiResponse.success(MemberIdDto.builder().id(memberReadService.myIdSearch(currentMember).getId()).build());
     }
 
     @GetMapping("/{id}")
     public ApiResponse<MemberInfoDto> getMember(@PathVariable Long id, @AuthenticationPrincipal CustomOAuth2User currentMember) {
-        return ApiResponse.success(memberReadService.memberSearch(MemberRequestDto.builder().id(id).currentId(currentMember.getId()).build()));
+        return ApiResponse.success(memberReadService.memberSearch(MemberRequestDto.builder()
+                .id(id)
+                .currentId(memberReadService.myIdSearch(currentMember).getId())
+                .build()));
     }
 
     @PutMapping("/{id}")
@@ -45,7 +48,7 @@ public class MemberController {
 
     @DeleteMapping("/{id}")
     public ApiResponse<DeleteMemberSuccessDto> deleteMember(@PathVariable Long id, @AuthenticationPrincipal CustomOAuth2User currentMember) {
-        return ApiResponse.success(memberWriteService.deleteMember(MemberRequestDto.builder().id(id).currentId(memberRepository.findBySso(currentMember.getUsername()).getId()).build()));
+        return ApiResponse.success(memberWriteService.deleteMember(MemberRequestDto.builder().id(id).currentId(memberReadService.myIdSearch(currentMember).getId()).build()));
     }
 
     @GetMapping("/validate-nickname/{nickname}")
