daa681b7580433117050608bdf7007f996a6d5b4
inhohyun <inhohyun36@gmail.com>
2024-08-07 10:29:59 +0900

    fix: 팔로워 팔로잉 파일 잘못 수정한거 되돌림

--- front/ott/src/components/userPage/Followers.jsx
+++ front/ott/src/components/userPage/Followers.jsx
@@ -1,62 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import defaultImage from '../../assets/icons/main.logo.png';
-import { getFollowingList } from '../../api/following/following'; // 팔로잉 목록을 가져오는 API
-
-const Following = ({ uid }) => {
-  const [following, setFollowing] = useState([]);
-  const [visibleFollowing, setVisibleFollowing] = useState(10);
+import { getFollowerList } from '../../api/follower/follower';
+const Followers = ({ uid }) => {
+  const [followers, setFollowers] = useState([]);
+  const [visibleFollowers, setVisibleFollowers] = useState(10);
 
   useEffect(() => {
-    const fetchFollowing = async () => {
+    const fetchFollowers = async () => {
       try {
-        const response = await getFollowingList(uid); // uid를 사용하여 팔로잉 목록을 가져옵니다
+        const response = await getFollowerList(uid); // TODO : props로 전달 받을 때 한 번 타고 들어가는거 수정
         console.log('response:', response);
-        setFollowing(response.data);
+        setFollowers(response.data);
       } catch (error) {
-        console.error('팔로잉 목록을 불러오는 중 오류 발생:', error);
+        console.error('팔로워 목록을 불러오는 중 오류 발생:', error);
       }
     };
 
-    fetchFollowing();
+    fetchFollowers();
   }, [uid]);
 
   const handleShowMore = () => {
-    setVisibleFollowing((prev) => prev + 10); // Show 10 more following
+    setVisibleFollowers((prev) => prev + 10); // Show 10 more followers
   };
 
   return (
     <div className="flex flex-col items-center mb-20">
       <div className="backdrop-blur-md bg-white bg-opacity-40 text-stone-500 p-6 rounded-2xl max-w-md w-full h-full overflow-y-auto">
         <div className="flex justify-center items-center mb-4 relative">
-          <h3 className="text-xl font-bold">팔로잉 목록</h3>
+          <h3 className="text-xl font-bold">팔로워 목록</h3>
         </div>
         <div className="flex flex-col space-y-4">
           {Array.from(
-            { length: Math.ceil(visibleFollowing / 2) },
+            { length: Math.ceil(visibleFollowers / 2) },
             (_, rowIndex) => (
               <div key={rowIndex} className="flex justify-center space-x-4">
-                {following
+                {followers
                   .slice(rowIndex * 2, rowIndex * 2 + 2)
-                  .map((follow, index) => (
+                  .map((follower, index) => (
                     <div
                       key={index}
                       className="flex items-center space-x-4 w-1/2 justify-center"
                     >
                       <div className="w-12 h-12 bg-gray-300 rounded-full border border-gray-300 overflow-hidden">
                         <img
-                          src={follow.image || defaultImage}
-                          alt="Following"
+                          src={follower.image || defaultImage}
+                          alt="Follower"
                           className="w-full h-full object-cover"
                         />
                       </div>
-                      <span>{follow.nickname}</span>
+                      <span>{follower.nickname}</span>
                     </div>
                   ))}
               </div>
             )
           )}
         </div>
-        {visibleFollowing < following.length && (
+        {visibleFollowers < followers.length && (
           <p
             onClick={handleShowMore}
             className="mt-4 text-stone-500 py-2 px-5 rounded-full cursor-pointer text-center bg-gray-200"
@@ -69,4 +68,4 @@ const Following = ({ uid }) => {
   );
 };
 
-export default Following;
+export default Followers;
