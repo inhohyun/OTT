b317ccf89c08733ee02c82b0869b63fb10cdda03
inhohyun <inhohyun36@gmail.com>
2024-08-07 15:15:36 +0900

    fix: filteredResults 변수 잘못 연결됨 수정

--- front/ott/src/pages/Search/SearchPage.jsx
+++ front/ott/src/pages/Search/SearchPage.jsx
@@ -146,7 +146,7 @@ const SearchPage = () => {
         item.title.toLowerCase().includes(searchQuery.toLowerCase())
       );
 
-      setResults(searchResult);
+      setResults(filteredResults);
     }
   };
 
