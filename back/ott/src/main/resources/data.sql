659018ad0a90748d9503cc674b683b76e1daceb6
JiEung2 <wldmd5007@naver.com>
2024-08-07 10:22:28 +0900

    fix: 구조 변경으로 인한 초기값 삭제

--- back/ott/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO category (name) VALUES ('상의') ON DUPLICATE KEY UPDATE name=name;
-INSERT INTO category (name) VALUES ('하의') ON DUPLICATE KEY UPDATE name=name;
-INSERT INTO category (name) VALUES ('아우터') ON DUPLICATE KEY UPDATE name=name;
