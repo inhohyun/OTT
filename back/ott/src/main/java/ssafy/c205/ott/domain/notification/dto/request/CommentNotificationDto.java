990552d61e9e7cb3adab999193cf0ebd65847ef7
JiEung2 <181757@jnu.ac.kr>
2024-08-08 23:58:40 +0900

    feat: CommentNotificationDto 생성

---
+++ back/ott/src/main/java/ssafy/c205/ott/domain/notification/dto/request/CommentNotificationDto.java
@@ -0,0 +1,16 @@
+package ssafy.c205.ott.domain.notification.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import ssafy.c205.ott.domain.notification.entity.NotificationType;
+
+@Getter
+@Builder
+public class CommentNotificationDto {
+
+    private NotificationType notificationType;
+    private Long memberId;
+    private Long commentId;
+    private Long commentAuthorId;
+    private String commentAuthorName;
+}
