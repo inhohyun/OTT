fb0e9cebf47d8e5ea40122d3f39dac7163656b7f
JiEung2 <181757@jnu.ac.kr>
2024-08-08 23:57:01 +0900

    fix: Notification 확장을 위한 수정
    - NotificationType 추가
    - BaseEntity 추가
    - 알림을 받는 대상인 memberId 추가

--- back/ott/src/main/java/ssafy/c205/ott/domain/notification/entity/Notification.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/notification/entity/Notification.java
@@ -1,14 +1,18 @@
 package ssafy.c205.ott.domain.notification.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.antlr.v4.runtime.misc.NotNull;
-import ssafy.c205.ott.domain.lookbook.entity.Comment;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import ssafy.c205.ott.common.entity.BaseEntity;
 
-@Entity @Getter
-public class Notification {
-    //Todo: 알림을 통해서 comment로 이동했을 때, comment가 삭제되었다면 예외처리
+@Entity
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,24 +21,14 @@ public class Notification {
     private String message;
 
     @Enumerated(EnumType.STRING)
-    NotificationStatus notificationStatus = NotificationStatus.UNREAD;
+    @Column(nullable = false)
+    NotificationStatus notificationStatus;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "comment_id")
-    private Comment comment;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    NotificationType notificationType;
 
-    private Long memberid;
+    @Column(nullable = false)
+    private Long memberId;
 
-    @Builder
-    public Notification(Long id, String message, NotificationStatus notificationStatus, Comment comment, Long memberid) {
-        this.id = id;
-        this.message = message;
-        this.notificationStatus = notificationStatus;
-        this.comment = comment;
-        this.memberid = memberid;
-    }
-
-    public Notification() {
-
-    }
 }
