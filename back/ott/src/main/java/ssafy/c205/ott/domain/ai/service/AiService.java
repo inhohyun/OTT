9dfd134b2b0c84b6b91c16b9868c4d78250b0743
JiEung2 <181757@jnu.ac.kr>
2024-08-09 00:04:33 +0900

    feat: AI 서비스가 완료되었을 때 알림 생성하는 코드 추가

--- back/ott/src/main/java/ssafy/c205/ott/domain/ai/service/AiService.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/ai/service/AiService.java
@@ -15,7 +15,7 @@ import ssafy.c205.ott.common.exception.S3FileNotExistException;
 import ssafy.c205.ott.common.util.AmazonS3Util;
 import ssafy.c205.ott.domain.ai.dto.AiRequestDto;
 import ssafy.c205.ott.domain.ai.exception.AiBadRequestException;
-import ssafy.c205.ott.domain.notification.util.NotificationMessage;
+import ssafy.c205.ott.domain.notification.dto.request.AiNotificationDto;
 import ssafy.c205.ott.domain.notification.service.NotificationService;
 
 import java.util.HashMap;
@@ -89,7 +89,7 @@ public class AiService {
         amazonS3Util.deleteFile(aiRequestDto.getModelImagePath());
 
         // AI 이미지 완성 알림
-        notificationService.createNotification(NotificationMessage.AI_COMPLETE, aiRequestDto.getMemberId());
+        notificationService.createAiNotification(AiNotificationDto.builder().memberId(aiRequestDto.getMemberId()).build());
 
         return ResponseEntity.ok(responseBody);
     }
