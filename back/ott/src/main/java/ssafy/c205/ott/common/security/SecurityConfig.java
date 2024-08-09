5d769cf5a6a7b91e1cf108efff07079b1ee1aed8
JaeHunJang <wuddlaa@gmail.com>
2024-08-09 15:47:40 +0900

    fix: add swagger permit

--- back/ott/src/main/java/ssafy/c205/ott/common/security/SecurityConfig.java
+++ back/ott/src/main/java/ssafy/c205/ott/common/security/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
             .authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/login","/", "/api/reissue", "/oauth2/authorization/**", "/api/login/**", "/swagger-ui/**").permitAll()
+                .requestMatchers("/login","/", "/api/reissue", "/oauth2/authorization/**", "/api/login/**", "/swagger-ui/**", "/api-docs/**").permitAll()
                 .anyRequest().authenticated());
 
         http
