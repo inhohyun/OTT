bd589b6a4a0d8cd2699be46a2c397f993f27f60f
JiEung2 <wldmd5007@naver.com>
2024-08-09 14:54:46 +0900

    fix: AiNotification NoArgsConstructor 삭제

--- back/ott/src/main/java/ssafy/c205/ott/domain/notification/entity/AiNotification.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/notification/entity/AiNotification.java
@@ -3,13 +3,11 @@ package ssafy.c205.ott.domain.notification.entity;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @SuperBuilder
 @AllArgsConstructor
-@NoArgsConstructor
 public class AiNotification extends Notification{
 }
