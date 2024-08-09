eb91e3e1bbbf1d83fbdb7b4fe5a62e9e9105a743
JiEung2 <wldmd5007@naver.com>
2024-08-09 15:35:13 +0900

    fix: 인가에 swagger 추가

--- back/ott/src/main/java/ssafy/c205/ott/common/security/SecurityConfig.java
+++ back/ott/src/main/java/ssafy/c205/ott/common/security/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
             .authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/login","/", "/api/reissue", "/oauth2/authorization/**", "/api/login/**").permitAll()
+                .requestMatchers("/login","/", "/api/reissue", "/oauth2/authorization/**", "/api/login/**", "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated());
 
         http
