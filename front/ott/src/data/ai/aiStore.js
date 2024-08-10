367590838ecd9d59213f432bbd09f656cfc6727e
inhohyun <inhohyun36@gmail.com>
2024-08-10 14:51:25 +0900

    fix: 프로그래스바 진행 끊김 현상 해결

--- front/ott/src/data/ai/aiStore.js
+++ front/ott/src/data/ai/aiStore.js
@@ -1,19 +1,23 @@
 import { create } from 'zustand';
 import defaultImage from '@/assets/images/default_picture.png';
+import pants1 from '@/assets/images/clothes/pants1.jpg';
+import pants1Back from '@/assets/images/clothes/pants1-1.jpg';
+import pants2 from '@/assets/images/clothes/pants2.jpg';
+import pants2Back from '@/assets/images/clothes/pants2-1.jpg';
+import pants3 from '@/assets/images/clothes/pants3.jpg';
+import pants3Back from '@/assets/images/clothes/pants3-1.jpg';
+import shirt1 from '@/assets/images/clothes/shirt1.jpg';
+import shirt1Back from '@/assets/images/clothes/shirt1-1.jpg';
+import shirt2 from '@/assets/images/clothes/shirt2.jpg';
+import shirt2Back from '@/assets/images/clothes/shirt2-1.jpg';
+import shirt3 from '@/assets/images/clothes/shirt3.jpg';
+import shirt3Back from '@/assets/images/clothes/shirt3-1.jpg';
 
 const useStore = create((set, get) => ({
-  // interval ID를 상태에 저장
-  intervalId: null,
-  // 피팅 진행도
-  percentage: 0,
-  // 타겟 퍼센티지와 인크리먼트 설정
-  targetPercentage: 99,
-  duration: 50,
-  increment: 99 / 50,
-  interval: 1000, // 1초마다 업데이트
-
   // 현재 페이지
   currentStep: 'InitialScreen',
+  // 피팅 진행도
+  percentage: 0,
   // 모달 상태
   isModalVisible: false,
   // 현재 옷이 선택되어 있는지 여부
@@ -25,10 +29,59 @@ const useStore = create((set, get) => ({
   filter: '상의',
   sample: 4,
   selectedClothesURL: null,
-  resultImages: null,
 
-  // 상태를 설정하는 메소드들
+  // 생성된 옷 이미지 데이터 저장
+  resultImages: null,
+  clothes: [
+    {
+      id: 1,
+      category: '하의',
+      frontImage: pants1,
+      backImage: pants1Back,
+      isLiked: false,
+    },
+    {
+      id: 2,
+      category: '하의',
+      frontImage: pants2,
+      backImage: pants2Back,
+      isLiked: false,
+    },
+    {
+      id: 3,
+      category: '하의',
+      frontImage: pants3,
+      backImage: pants3Back,
+      isLiked: false,
+    },
+    {
+      id: 4,
+      category: '상의',
+      frontImage: shirt1,
+      backImage: shirt1Back,
+      isLiked: false,
+    },
+    {
+      id: 5,
+      category: '상의',
+      frontImage: shirt2,
+      backImage: shirt2Back,
+      isLiked: false,
+    },
+    {
+      id: 6,
+      category: '상의',
+      frontImage: shirt3,
+      backImage: shirt3Back,
+      isLiked: false,
+    },
+  ],
+
+  // 상태 설정 메서드들
   setCurrentStep: (step) => set({ currentStep: step }),
+  setPercentage: (updater) =>
+    set((state) => ({ percentage: updater(state.percentage) })),
+  resetPercentage: () => set({ percentage: 0 }),
   setIsModalVisible: (isVisible) => set({ isModalVisible: isVisible }),
   setSelectedClothing: (clothing) => set({ selectedClothing: clothing }),
   setFilter: (filter) => set({ filter }),
@@ -36,49 +89,39 @@ const useStore = create((set, get) => ({
   setModelImage: (modelImage) => set({ modelImage }),
   setSelectedImage: (selectedImage) => set({ selectedImage }),
   setModelPicture: (modelPicture) => set({ modelPicture }),
-  setSelectedClothesURL: (selectedClothesURL) => set({ selectedClothesURL }),
+  setselectedClothesURL: (selectedClothesURL) => set({ selectedClothesURL }),
   setResultImages: (resultImages) => set({ resultImages }),
+  toggleLike: (id) =>
+    set((state) => ({
+      clothes: state.clothes.map((item) =>
+        item.id === id ? { ...item, isLiked: !item.isLiked } : item
+      ),
+    })),
 
-  // 인터벌을 시작하는 메소드
+  // 프로그래스 바 진행 관리 메서드들
+  intervalId: null,
   startInterval: () => {
     const state = get();
     if (state.intervalId) return; // 이미 interval이 실행 중이면 무시
 
     const id = setInterval(() => {
       set((state) => {
-        const nextPercentage = state.percentage + state.increment;
-        if (nextPercentage >= state.targetPercentage) {
+        const nextPercentage = state.percentage + 99 / 50; // 인크리먼트 값은 99/50
+        if (nextPercentage >= 99) {
           clearInterval(state.intervalId);
           return { percentage: 99, intervalId: null };
         }
         return { percentage: nextPercentage };
       });
-    }, state.interval);
+    }, 1000); // 1초마다 업데이트
 
     set({ intervalId: id });
   },
 
-  // 인터벌을 클리어하는 메소드
   clearInterval: () => {
     const state = get();
     clearInterval(state.intervalId);
     set({ intervalId: null });
-  },
-
-  // 수동으로 퍼센티지 상태를 리셋할 수 있는 메소드
-  resetPercentage: () => {
-    set({ percentage: 0 });
-  },
-
-  // 새로운 메서드: 프로그래스를 직접 업데이트
-  updateProgress: (value) => {
-    const state = get();
-    if (state.percentage + value >= state.targetPercentage) {
-      set({ percentage: state.targetPercentage, intervalId: null });
-      clearInterval(state.intervalId);
-    } else {
-      set({ percentage: state.percentage + value });
-    }
   },
 }));
 
