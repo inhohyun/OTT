14a4b04cc7e409fb0f9cd8894d81190ad0bf709e
최승현 <csh7099@naver.com>
2024-08-11 16:49:31 +0900

    fix : lookbook detail response dto

--- back/ott/src/main/java/ssafy/c205/ott/domain/lookbook/dto/responsedto/LookbookDetailDto.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/lookbook/dto/responsedto/LookbookDetailDto.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Setter
 @ToString
 public class LookbookDetailDto {
+
     private long viewCount;
     private String content;
     private String nickname;
@@ -23,9 +24,15 @@ public class LookbookDetailDto {
     private int cntFavorite;
     private int cntComment;
     private boolean isFavorite;
+    private String profileImg;
+    private boolean isFollow;
 
     @Builder
-    public LookbookDetailDto(long viewCount, String content, String nickname, List<ClothesImageDto> images, List<String> tags, List<ClothesImageDto> salesClothes, LocalDateTime createdAt, String thumnail, int cntFavorite,int cntComment, boolean isFavorite) {
+    public LookbookDetailDto(long viewCount, String content, String nickname,
+        List<ClothesImageDto> images, List<String> tags, List<ClothesImageDto> salesClothes,
+        LocalDateTime createdAt, String thumnail, int cntFavorite, int cntComment,
+        boolean isFavorite,
+        String profileImg, boolean isFollow) {
         this.viewCount = viewCount;
         this.content = content;
         this.nickname = nickname;
@@ -35,8 +42,10 @@ public class LookbookDetailDto {
         this.createdAt = createdAt;
         this.thumnail = thumnail;
         this.cntFavorite = cntFavorite;
-        this.isFavorite = isFavorite;
         this.cntComment = cntComment;
+        this.isFavorite = isFavorite;
+        this.profileImg = profileImg;
+        this.isFollow = isFollow;
     }
 
     public LookbookDetailDto() {
--- back/ott/src/main/java/ssafy/c205/ott/domain/lookbook/service/LookbookServiceImpl.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/lookbook/service/LookbookServiceImpl.java
@@ -19,6 +19,7 @@ import ssafy.c205.ott.common.entity.PublicStatus;
 import ssafy.c205.ott.common.util.AmazonS3Util;
 import ssafy.c205.ott.domain.account.entity.Follow;
 import ssafy.c205.ott.domain.account.entity.Member;
+import ssafy.c205.ott.domain.account.repository.FollowRepository;
 import ssafy.c205.ott.domain.account.repository.MemberRepository;
 import ssafy.c205.ott.domain.item.entity.Item;
 import ssafy.c205.ott.domain.item.entity.ItemImage;
@@ -63,6 +64,7 @@ public class LookbookServiceImpl implements LookbookService {
     private final AmazonS3Util amazonS3Util;
     private final LookbookImageRepository lookbookImageRepository;
     private final CommentService commentService;
+    private final FollowRepository followRepository;
 
     @Override
     public void createLookbook(LookbookDto lookbookCreateDto, MultipartFile file) {
@@ -241,6 +243,9 @@ public class LookbookServiceImpl implements LookbookService {
                 isFavorite = true;
             }
 
+            boolean isFollow = followRepository.existsByToMemberIdAndFromMemberId(
+                saveLookbook.getMember().getId(), memberId);
+
             return LookbookDetailDto
                 .builder()
                 .content(saveLookbook.getContent())
@@ -254,6 +259,8 @@ public class LookbookServiceImpl implements LookbookService {
                 .cntFavorite(cnt)
                 .isFavorite(isFavorite)
                 .cntComment(commentService.countComment(lookbookId))
+                .profileImg(saveLookbook.getMember().getProfileImageUrl())
+                .isFollow(isFollow)
                 .build();
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, lookbookId + "의 룩북을 찾지 못했습니다.");
@@ -614,6 +621,7 @@ public class LookbookServiceImpl implements LookbookService {
                 .img(lookbook.getLookbookImages().get(0).getImageUrl())
                 .cntFavorite(cntLikeLookbook(String.valueOf(lookbook.getId())))
                 .cntComment(commentService.countComment(String.valueOf(lookbook.getId())))
+                .createdAt(lookbook.getCreatedAt())
                 .tags(lookbook.getLookbookTags().stream()
                     .map(lookbookTag -> lookbookTag.getTag().getName()).toArray(String[]::new))
                 .isFavorite(isFavorite)
