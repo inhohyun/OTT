6ace3d68f197c5cd79e1516e6293cf338c5506d8
inhohyun <inhohyun36@gmail.com>
2024-08-12 17:13:14 +0900

    fix: 메인 페이지 memberId 콘솔

--- front/ott/src/pages/MainPage/MainPage.jsx
+++ front/ott/src/pages/MainPage/MainPage.jsx
@@ -43,6 +43,7 @@ const MainPage = () => {
     const fetchFollowCount = async () => {
       // console.log('Fetching follow count...'); // 이 로그가 찍히는지 확인
       try {
+        console.log('메인페이지에서 보내는:', memberId);
         const response = await getFollowingCount(memberId);
         // console.log('API Response:', response); // 이 로그가 찍히는지 확인
         if (response.data.data !== 0) {
