52e1fe95cc026da53bf055332d2fbeccf15c2dd5
inhohyun <inhohyun36@gmail.com>
2024-08-13 16:42:27 +0900

    fix: top level await remove

--- front/ott/src/api/notification/pushNotification.js
+++ front/ott/src/api/notification/pushNotification.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
-import axios, { HttpStatusCode } from "axios";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+import axios, { HttpStatusCode } from 'axios';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,7 +16,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FCM_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FCM_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FCM_APP_ID,
-  measurementId: import.meta.env.VITE_FCM_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_FCM_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -26,39 +26,39 @@ const messaging = getMessaging(app);
 const baseUrl = import.meta.env.VITE_REACT_APP_API_BASE_URL;
 
 async function requestPermission() {
-    console.log("권한 요청 중...");
-  
-    const permission = await Notification.requestPermission();
-    if (permission === "denied") {
-      console.log("알림 권한 허용 안됨");
-      return;
-    }
-  
-    console.log("알림 권한이 허용됨");
+  console.log('권한 요청 중...');
 
-    const token = await getToken(messaging, {
-        vapidKey: import.meta.env.VITE_FCM_VAPID_KEY,
-    });  
-    if (token) {
-        console.log("token: ", token);
-        axios.post(baseUrl+"api/push/device", {memberId:1, token:token})
-        .then(response => {
-            if (response.status == HttpStatusCode.Ok) {
-                console.log("알림 등록 성공");
-            } else {
-                console.log("알림 등록 실패");
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    }
-    else console.log("Can not get Token");
-  
-    onMessage(messaging, (payload) => {
-      console.log("메시지가 도착했습니다.", payload);
-      // ...
-    });
+  const permission = await Notification.requestPermission();
+  if (permission === 'denied') {
+    console.log('알림 권한 허용 안됨');
+    return;
   }
-  
-  await requestPermission();
+
+  console.log('알림 권한이 허용됨');
+
+  const token = await getToken(messaging, {
+    vapidKey: import.meta.env.VITE_FCM_VAPID_KEY,
+  });
+  if (token) {
+    console.log('token: ', token);
+    axios
+      .post(baseUrl + 'api/push/device', { memberId: 1, token: token })
+      .then((response) => {
+        if (response.status == HttpStatusCode.Ok) {
+          console.log('알림 등록 성공');
+        } else {
+          console.log('알림 등록 실패');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else console.log('Can not get Token');
+
+  onMessage(messaging, (payload) => {
+    console.log('메시지가 도착했습니다.', payload);
+    // ...
+  });
+}
+
+requestPermission();
