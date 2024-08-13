015a2f7e9283e43ba223c07dfc11711021452066
JiEung2 <wldmd5007@naver.com>
2024-08-13 11:47:19 +0900

    feat: FCMPushNotificationDto에 Builder 추가

--- back/ott/src/main/java/ssafy/c205/ott/domain/notification/dto/requestdto/FCMPushNotificationDto.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/notification/dto/requestdto/FCMPushNotificationDto.java
@@ -1,8 +1,10 @@
 package ssafy.c205.ott.domain.notification.dto.requestdto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class FCMPushNotificationDto {
     private Long memberId;
     private String title;
