cad91168ae689edffb152d208f8a88fb8a300502
최승현 <csh7099@naver.com>
2024-08-14 17:47:58 +0900

    feat : Set Spring Timezone

--- back/ott/src/main/java/ssafy/c205/ott/OttApplication.java
+++ back/ott/src/main/java/ssafy/c205/ott/OttApplication.java
@@ -1,5 +1,7 @@
 package ssafy.c205.ott;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +12,11 @@ public class OttApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OttApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
