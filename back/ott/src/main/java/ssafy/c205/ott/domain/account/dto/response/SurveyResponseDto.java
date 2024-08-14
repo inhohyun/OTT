4d7fd61cda1fc92e6bbd17451ce2f8b59a4507e8
JiEung2 <wldmd5007@naver.com>
2024-08-14 13:56:48 +0900

    feat: SurveyResponseDto 생성

---
+++ back/ott/src/main/java/ssafy/c205/ott/domain/account/dto/response/SurveyResponseDto.java
@@ -0,0 +1,10 @@
+package ssafy.c205.ott.domain.account.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SurveyResponseDto {
+    boolean surveyCompleted;
+}
