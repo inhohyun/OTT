ba7d58cf9ede98b72401693d64cb32afe5d2979c
최승현 <csh7099@naver.com>
2024-08-14 10:21:11 +0900

    feat : RTC Closet Clothes Search

--- back/ott/src/main/java/ssafy/c205/ott/domain/item/repository/ItemCategoryRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/item/repository/ItemCategoryRepository.java
@@ -5,9 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ssafy.c205.ott.common.entity.ItemCategory;
+import ssafy.c205.ott.common.entity.PublicStatus;
 
 public interface ItemCategoryRepository extends JpaRepository<ItemCategory, Long> {
+
     @Query("SELECT ic FROM ItemCategory ic WHERE ic.item.member.id = :memberId AND ic.category.id = :categoryId")
-    List<ItemCategory> findByMemberIdAndCategoryId(@Param("memberId") Long memberId, @Param("categoryId") Long categoryId);
+    List<ItemCategory> findByMemberIdAndCategoryId(@Param("memberId") Long memberId,
+        @Param("categoryId") Long categoryId);
+
     List<ItemCategory> findByCategoryId(Long categoryId);
+
+    @Query("SELECT ic FROM ItemCategory ic WHERE ic.item.member.id = :memberId AND ic.category.id = :categoryId And ic.item.publicStatus = :publicStatus")
+    List<ItemCategory> findByMemberIdAndCategoryIdAndPublicStatus(@Param("memberId") Long memberId,
+        @Param("categoryId") Long categoryId, @Param("publicStatus") PublicStatus publicStatus);
 }
--- back/ott/src/main/java/ssafy/c205/ott/domain/item/repository/ItemRepository.java
+++ back/ott/src/main/java/ssafy/c205/ott/domain/item/repository/ItemRepository.java
@@ -2,10 +2,12 @@ package ssafy.c205.ott.domain.item.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ssafy.c205.ott.common.entity.PublicStatus;
 import ssafy.c205.ott.domain.item.entity.BookmarkStatus;
 import ssafy.c205.ott.domain.item.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByMemberId(Long memberId);
     List<Item> findByMemberIdAndBookmarkStatus(Long memberId, BookmarkStatus bookmarkStatus);
+    List<Item> findByMemberIdAndPublicStatus(Long memberId, PublicStatus publicStatus);
 }
