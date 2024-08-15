3e22b8cf47631aafde499e65f1ceb341d6ffd556
inhohyun <inhohyun36@gmail.com>
2024-08-15 12:53:07 +0900

    test: ai 옷장 정상동작 테스트

--- front/ott/src/data/ai/bookmarkClothes.js
+++ front/ott/src/data/ai/bookmarkClothes.js
@@ -3,20 +3,27 @@ import { create } from 'zustand';
 const bookmarkClothes = create((set) => ({
   bookmarkedClothes: [],
 
-  // 선택된 옷을 ai 옷장에서 제거하는 함수
-  removeBookmarkedClothes: (clothesToRemove) =>
-    set((state) => ({
-      bookmarkedClothes: state.bookmarkedClothes.filter(
+  // AI 옷장에서 선택된 옷을 제거하는 함수
+  removeBookmarkedClothes: (clothesToRemove) => {
+    console.log('제거할 옷의 ID:', clothesToRemove.clothesId);
+    set((state) => {
+      const updatedClothes = state.bookmarkedClothes.filter(
         (clothes) => clothes.clothesId !== clothesToRemove.clothesId
-      ),
-    })),
+      );
+      console.log('제거 후 북마크된 옷 목록:', updatedClothes);
+      return { bookmarkedClothes: updatedClothes };
+    });
+  },
 
-  // 선택된 옷을 ai 옷장에 추가하는 함수
-  setBookmarkedClothes: (newClothes) =>
-    set((state) => ({
-      // 여기서 newClothes가 객체이므로, 배열에 직접 추가
-      bookmarkedClothes: [...state.bookmarkedClothes, newClothes],
-    })),
+  // AI 옷장에 선택된 옷을 추가하는 함수
+  setBookmarkedClothes: (newClothes) => {
+    console.log('추가할 새 옷:', newClothes);
+    set((state) => {
+      const updatedClothes = [...state.bookmarkedClothes, newClothes];
+      console.log('추가 후 북마크된 옷 목록:', updatedClothes);
+      return { bookmarkedClothes: updatedClothes };
+    });
+  },
 }));
 
 export default bookmarkClothes;
