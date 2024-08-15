28297efb616bcd02fcc8facb3985e5c21068f94a
JaeHunJang <wuddlaa@gmail.com>
2024-08-16 03:43:09 +0900

    fix: lookbook image url purge cache param

--- front/ott/src/components/lookbook/CanvasArea.jsx
+++ front/ott/src/components/lookbook/CanvasArea.jsx
@@ -33,7 +33,7 @@ const CanvasArea = ({
             style={{ left: item.x, top: item.y }}
           >
             <img
-              src={(item.imagePath?.path || item.image) + "?" + new Date().getTime()}
+              src={(item.imagePath?.path || item.image) + "?d=" + new Date().getTime()}
               alt={item.name}
               className="w-full h-full cursor-move"
               onMouseDown={() => handleMouseDown(index)}
