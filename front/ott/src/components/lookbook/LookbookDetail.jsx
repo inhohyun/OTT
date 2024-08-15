03b4f223b2b5dbd93694c26a6b9ae8077d3734d6
gahyoenj <rkgus678@gmail.com>
2024-08-15 13:58:38 +0900

    fix: 룩북 디테일 팔로우 수정

--- front/ott/src/components/lookbook/LookbookDetail.jsx
+++ front/ott/src/components/lookbook/LookbookDetail.jsx
@@ -147,14 +147,14 @@ const LookbookDetail = ({
   const toggleFollow = () => {
     if (followed) {
       try {
-        unfollowUser(currentLookbook.memberId);
+        unfollowUser(lookbook.memberId);
         setFollowed(false);
       } catch (error) {
         console.error(error);
       }
     } else {
       try {
-        followUser(currentLookbook.memberId);
+        followUser(lookbook.memberId);
         setFollowed(true);
       } catch (error) {
         console.error(error);
