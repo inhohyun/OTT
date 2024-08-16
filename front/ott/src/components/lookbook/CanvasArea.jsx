1f44925151e9ef19a387ad2317516f94aba66f39
전가현 <rkgus678@gmail.com>
2024-08-16 11:46:03 +0900

    fix: 룩북 수정 시 이미지 안불러와지는 오류

--- front/ott/src/components/lookbook/CanvasArea.jsx
+++ front/ott/src/components/lookbook/CanvasArea.jsx
@@ -33,7 +33,7 @@ const CanvasArea = ({
             style={{ left: item.x, top: item.y }}
           >
             <img
-              src={item.image?.path || item.imagePath || item.imagePath.path}
+              src={item.image?.path || item.imagePath.path || item.imagePath}
               alt={item.name}
               className="w-full h-full cursor-move"
               onMouseDown={() => handleMouseDown(index)}
